d32f403511b0b269c7d9f6585988e558c3f6de2b
Pranav-KB14 <pranav.kb@skysecure.ai>
2025-11-12 17:23:14 +0530

    Core values of all the 4 pages allignment changed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -14,7 +14,7 @@ const GLOBAL_OFFICES = [
     address: '30 Cecil Street, # 19-08, Prudential Tower, Singapore - 049712'
   },
   {
-    city: 'Deira, UAE',
+    city: 'Deira, Dubai',
     address: 'Office No. 110, Sheikh Rashid Rd - Al Garhoud, Deira, Dubai, United Arab Emirates'
   }
 ];
